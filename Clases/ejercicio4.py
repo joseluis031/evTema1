fb086e5d687067f64b9f61a6ae91039aad21bd25
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-23 18:02:28 +0200

    commit

--- Clases/ejercicio4.py
+++ Clases/ejercicio4.py
@@ -1,2 +1,15 @@
-f =print(int(input("introduce el numero de filas")))
-c =print(int(input("introduce el numero de columnas")))
+f =int(input("introduce el numero de filas"))
+c =int(input("introduce el numero de columnas"))
+
+def tabla(f,c):
+    
+    if f<1 or f>9:
+        print("Error, filas/index out of range...")
+    else:
+        pass
+    if c<1 or c>9:
+        print("Error, columnas/index out of range...")
+    else:
+        pass
+
+tabla(f,c)
