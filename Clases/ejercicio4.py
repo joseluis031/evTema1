dc2f216bdce1d608b65b185e0cf398a637475229
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-23 18:25:13 +0200

    avanzando

--- Clases/ejercicio4.py
+++ Clases/ejercicio4.py
@@ -1,15 +1,19 @@
+from tabulate import tabulate
+
 f =int(input("introduce el numero de filas: "))
 c =int(input("introduce el numero de columnas: "))
-
-def tabla(f,c):
-    
+d=[]
+def tabla(f,c,d):
+    d = []
     if f<1 or f>9 and c<1 or c>9 :
         print("Error, index out of range...")
     
     elif c<1 or c>9 or f<1 or f>9:
         print("Error, index out of range...")
     else:
+        d.append(f)
+        d.append(c)
+        print(d)
         
-        pass
 
-tabla(f,c)
+tabla(f,c,d)
