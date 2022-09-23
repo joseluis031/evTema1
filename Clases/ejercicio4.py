4d8aeab3eeae8110fc8f94b9b1ff86220333f93b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-23 18:06:05 +0200

    tengo los errores

--- Clases/ejercicio4.py
+++ Clases/ejercicio4.py
@@ -1,15 +1,15 @@
-f =int(input("introduce el numero de filas"))
-c =int(input("introduce el numero de columnas"))
+f =int(input("introduce el numero de filas: "))
+c =int(input("introduce el numero de columnas: "))
 
 def tabla(f,c):
     
-    if f<1 or f>9:
-        print("Error, filas/index out of range...")
+    if f<1 or f>9 and c<1 or c>9 :
+        print("Error, index out of range...")
+    
+    elif c<1 or c>9 or f<1 or f>9:
+        print("Error, index out of range...")
     else:
-        pass
-    if c<1 or c>9:
-        print("Error, columnas/index out of range...")
-    else:
+        
         pass
 
 tabla(f,c)
