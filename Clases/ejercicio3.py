92096f65d36b060c3f09c5b42df6b426b0d51b15
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-23 16:57:28 +0200

    pruebas

--- Clases/ejercicio3.py
+++ Clases/ejercicio3.py
@@ -7,7 +7,8 @@ print(list(range(0,55,5)))
 
 
 while True:
-    
-    x = [-10,0]
-    print(list(range(x)))
+    x = int(input())
+    x2 = int(input())
+    x3 = int(input())
+    print(list(range(x,x2,x3)))
     break
